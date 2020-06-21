6da539e14f7fb047e691301ddaa55035906b6d15
nmrozfrost <60041919+nmrozfrost@users.noreply.github.com>
2020-06-21 17:34:13 +0200

    Runner test

---
+++ src/main/java/destop/Runner.java
@@ -0,0 +1,19 @@
+package destop;
+
+import domain.Client;
+import persistence.ClientDAO;
+import persistence.ClientDAOImpl;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Runner {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("cinema");
+        ClientDAO clientDAO = new ClientDAOImpl(emf);
+
+        Client client1 = new Client(1, "Jan", "Kowlaski", "j.k@em.pl", "6666666");
+        clientDAO.save(client1);
+
+    }
+}
