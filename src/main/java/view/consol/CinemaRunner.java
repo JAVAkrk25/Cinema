d757adc9d20cc775f33dd6f4ff9246b069293ad7
Tarriamr <pawel.putala@gmail.com>
2020-06-27 12:33:04 +0200

    .

--- src/main/java/view/consol/CinemaRunner.java
+++ src/main/java/view/consol/CinemaRunner.java
@@ -5,7 +5,7 @@ import view.consol.graphicsPack.StartMenu;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Cinema {
+public class CinemaRunner {
     public static void main(String[] args) {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("SqtyJ7nmGK");
